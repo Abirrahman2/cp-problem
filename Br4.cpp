862671859bd24c4ba6c566b3afefc5d11217104d
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2023-01-30 20:54:32 +0600

    Add files via upload

---
+++ Br4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int d,spentTime;
+    cin>>d>>spentTime;
+    while(d--)
+    {
+        int minTime,maxTime;
+        cin>>minTime>>maxTime;
+    }
+}
